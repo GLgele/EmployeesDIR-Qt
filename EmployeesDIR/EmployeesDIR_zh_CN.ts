891a385f9ceb419ad4c7e99e8ab4c763d4ca5fb4
GLgele <GLgele@outlook.com>
2022-07-09 21:14:01 +0800

    [Renew] Qt Creator

--- EmployeesDIR/EmployeesDIR_zh_CN.ts
+++ EmployeesDIR/EmployeesDIR_zh_CN.ts
@@ -1,11 +1,3 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
-<context>
-    <name>EmployeesDIRClass</name>
-    <message>
-        <source>EmployeesDIR</source>
-        <translation type="unfinished">EmployeesDIR</translation>
-    </message>
-</context>
-</TS>
+<TS version="2.1" language="zh_CN"></TS>
--- EmployeesDIR/main.cpp
+++ EmployeesDIR/main.cpp
@@ -1,43 +1,23 @@
-#include "employeesdir.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+
+#include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-
-#include<json/json.h>
-#include<iostream>
-#include<fstream>
-#include <windows.h>
-#include <stdio.h>
-#include <stdarg.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    /*
+
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString& locale : uiLanguages) {
-        const QString baseName = "Hello_World_" + QLocale(locale).name();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "EmployeesDIR_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
         }
     }
-    */
-    EmployeesDIR w;
+    MainWindow w;
     w.show();
-    /*Json::Value root;
-    std::ifstream ifs;
-    ifs.open("a.json");
-    Json::CharReaderBuilder builder;
-    builder["collectComments"] = true;
-    JSONCPP_STRING errs;
-    if (!parseFromStream(builder, ifs, &root, &errs)) {
-        std::cout << errs << std::endl;
-        return EXIT_FAILURE;
-    }
-    std::cout << root << std::endl;
-    return EXIT_SUCCESS;*/
-    printf("Hello Qt!\n");
     return a.exec();
 }
